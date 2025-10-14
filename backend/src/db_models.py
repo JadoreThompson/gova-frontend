333774aee4375d0e20bccdf81e3115cbd138a9f1
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2025-10-14 22:11:38 +0100

    created projects table

--- backend/src/db_models.py
+++ backend/src/db_models.py
@@ -1,10 +1,14 @@
 from datetime import datetime
-from uuid import uuid4
+from uuid import uuid4, UUID
 
-from sqlalchemy import UUID, String, DateTime
+from sqlalchemy import UUID as SaUUID, String, DateTime
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 from utils.db import get_datetime
+
+
+def get_uuid():
+    return uuid4()
 
 
 class Base(DeclarativeBase):
@@ -15,10 +19,24 @@ class Users(Base):
     __tablename__ = "users"
 
     user_id: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid4
+        SaUUID(as_uuid=True), primary_key=True, default=get_uuid
     )
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, default=get_datetime
+    )
+
+
+class Projects(Base):
+    __tablename__ = "projects"
+
+    project_id: Mapped[UUID] = mapped_column(
+        SaUUID(as_uuid=True), primary_key=True, default=get_uuid
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    guidelines: Mapped[str] = mapped_column(String, nullable=True)
+    topics: Mapped[str] = mapped_column(String, nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, default=get_datetime
     )
