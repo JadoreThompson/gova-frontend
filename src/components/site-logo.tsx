a743db46dcbc7de007b3b00e2bb469e69fa265f5
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-04-22 22:05:03 +0100

    Added link to home in site logo

--- src/components/site-logo.tsx
+++ src/components/site-logo.tsx
@@ -1,9 +1,14 @@
 import type { FC } from "react";
 
 import logoWhite from "@/assets/logo_white.png";
+import { Link } from "react-router";
 
 const SiteLogo: FC<{ className?: string }> = (props) => {
-  return <img src={logoWhite} alt="Site Logo" className={props.className} />;
+  return (
+    <Link to="/" className="flex items-center justify-center">
+      <img src={logoWhite} alt="Site Logo" className={props.className} />
+    </Link>
+  );
 };
 
 export default SiteLogo;
